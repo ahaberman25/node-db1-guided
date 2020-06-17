1cd79f22dfd5debc53b547ea8a671becc93744f5
Andrew Haberman <a.haberman25@gmail.com>
2020-06-17 17:29:49 -0400

    guided done

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,60 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+    try {
+        // SELECT * FROM "messages";
+        const messages = await db.select("*").from("messages")
+        res.json(messages)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.get("/:id", (req, res, next) => {
-
+router.get("/:id", async (req, res, next) => {
+    try {
+        const message = await db.select("*").from("messages").where("id", req.params.id).limit(1)
+        res.json(message)
+    } catch (error) {
+        next(err)
+    }
 })
 
-router.post("/", (req, res, next) => {
-
+router.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+        const [messageID] = await db.insert(payload).into("messages")
+        const message= await db.first("*").from("messages").where("id", messageID)
+        res.status(201).json(message)
+    } catch (error) {
+        next(err)
+    }
 })
 
-router.put("/:id", (req, res, next) => {
-
+router.put("/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+        await db("messages").update(payload).where("id", req.params.id)
+        const message= await db.first("*").from("messages").where("id", req.params.id)
+        res.json(message)
+    } catch (error) {
+        next(err)
+    }
 })
 
-router.delete("/:id", (req, res, next) => {
-
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db("messages").where("id", req.params.id).del()
+        res.status(204).json({message: "message deleted"})
+    } catch (error) {
+        next(err)
+    }
 })
 
 module.exports = router
